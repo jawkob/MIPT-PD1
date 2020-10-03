3caf96dc323477efda0d632c13966c9e3c3acce5
jawkob <57799847+jawkob@users.noreply.github.com>
2020-10-03 17:26:46 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/hlwrld22/MainActivity.java
+++ app/src/main/java/com/example/hlwrld22/MainActivity.java
@@ -2,28 +2,18 @@ package com.example.hlwrld22;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView simple_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.simple_txt = findViewById(R.id.simple_txt);
     }
 
     public void onBtnChangeTextPress(View view) {
-        this.simple_txt.setText("beep beep boop boop");
-    }
 
-    public void onBtnChangeTextColorPress(View view) {
-        this.simple_txt.setTextColor(Color.RED);
     }
 }
