a5366b4e5f1392a142285c7bce7ae39c12388bbd
jawkob <57799847+jawkob@users.noreply.github.com>
2020-10-03 17:12:47 +0300

    lab1 first commit

--- app/src/main/java/com/example/hlwrld22/MainActivity.java
+++ app/src/main/java/com/example/hlwrld22/MainActivity.java
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView simple_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.simple_txt = findViewById(R.id.simple_txt);
     }
 
     public void onBtnChangeTextPress(View view) {
-
+        this.simple_txt.setText("beep beep boop boop");
     }
 }
